17042d7736d124a7d2978792627f94524dfd1867
Dawood Ahmad <dawoodahmadmct@gmail.com>
2024-09-06 17:10:10 +0100

    1. Project Setup 2. Nugets Added 3. Models Added

---
+++ SoldierTracker/Models/Location.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoldierTracker.Models
+{
+    public class Location
+    {
+        public int SensorID { get; set; }
+        public Sensor Sensor { get; set; }
+
+        public Guid SoldierID { get; set; }
+        public Soldier Soldier { get; set; }
+
+        public decimal Longitude { get; set; }
+        public decimal Latitude { get; set; }
+    }
+}
---
+++ SoldierTracker/Models/Sensor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoldierTracker.Models
+{
+    public class Sensor
+    {
+        [Key]
+        public int SensorID { get; set; }
+        public string SensorName { get; set; }
+        public string SensorType { get; set; }
+    }
+}
